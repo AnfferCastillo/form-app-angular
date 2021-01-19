b7bbb0689f5c8c98222c4b153106b1d7935d8f31
Anffer Castillo <anffer.castillo@gmail.com>
2021-01-18 23:11:25 -0300

    form

--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    ContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
